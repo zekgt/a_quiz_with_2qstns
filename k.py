f8bb56234132f8cfa71cfb44c5401d5cb3422cbc
zekgt <78991517+zekgt@users.noreply.github.com>
2021-03-07 22:58:15 +0800

    Add files via upload

---
+++ k.py
@@ -0,0 +1,19 @@
+#made by templar knight#3250 or Peezyte on Discord and Youtube
+print("Hello! This is a simple math quiz for you!")
+Answers1 = "10"
+print("Question 1 : What's 5 + 5?")
+Answers1 = int(input("Your Answer here :"))
+if Answers1 == 10:
+  print("You're Correct!")
+else:
+    print("You're wrong, the correct answer is actually 10!")
+Answers2 = "60"
+print("Question 2 : What's 30 x 2?")
+Answers2 = int(input("Your Answer here : "))
+if Answers2 == 60:
+    print("You're Correct!")
+else: 
+    print("You're wrong")
+print(" Okay that's all for today! DM me templar knight#3250 on discord for more content!")
+
+quit()
